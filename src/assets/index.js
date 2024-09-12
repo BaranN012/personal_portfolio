7623c938144d31a1e11427a7147a43c666d4a1dc
Baran Narravula <BaranNarravula@MacBook-Pro-171.lan>
2024-09-12 17:55:17 -0400

    changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
+import coderschool from "./company/coderschool.png";
 import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import apollo from "./company/apollo.png";
+import headstarter from "./company/headstarter.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +52,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
+  coderschool,
   shopify,
-  starbucks,
-  tesla,
+  apollo,
+  headstarter,
   carrent,
   jobit,
   tripguide,
--- src/constants/index.js
+++ src/constants/index.js
@@ -15,244 +15,241 @@ import {
     git,
     figma,
     docker,
-    meta,
-    starbucks,
-    tesla,
+    coderschool,
+    apollo,
+    headstarter,
     shopify,
     carrent,
     jobit,
     tripguide,
     threejs,
-    } from "../assets";
-    export const navLinks = [
+} from "../assets";
+
+export const navLinks = [
     {
-    id: "about",
-    title: "About",
+        id: "about",
+        title: "About",
     },
     {
-    id: "work",
-    title: "Work",
+        id: "work",
+        title: "Work",
     },
     {
-    id: "contact",
-    title: "Contact",
+        id: "contact",
+        title: "Contact",
     },
-    ];
-    const services = [
+];
+
+const services = [
     {
-    title: "Data Analyst",
-    icon: web,
-    },
-    {
-    title: "React Native Developer",
-    icon: mobile,
+        title: "Data Analyst",
+        icon: web,
     },
     {
-    title: "Backend Developer",
-    icon: backend,
+        title: "React Native Developer",
+        icon: mobile,
     },
     {
-    title: "Data Engineer",
-    icon: dataeng,
-    },
-    ];
-    const technologies = [
-    {
-    name: "HTML 5",
-    icon: html,
+        title: "Backend Developer",
+        icon: backend,
     },
     {
-    name: "CSS 3",
-    icon: css,
+        title: "Data Engineer",
+        icon: dataeng,
+    },
+];
+
+const technologies = [
+    {
+        name: "HTML 5",
+        icon: html,
     },
     {
-    name: "JavaScript",
-    icon: javascript,
+        name: "CSS 3",
+        icon: css,
     },
     {
-    name: "TypeScript",
-    icon: typescript,
+        name: "JavaScript",
+        icon: javascript,
     },
     {
-    name: "React JS",
-    icon: reactjs,
+        name: "TypeScript",
+        icon: typescript,
     },
     {
-    name: "Redux Toolkit",
-    icon: redux,
+        name: "React JS",
+        icon: reactjs,
     },
     {
-    name: "Tailwind CSS",
-    icon: tailwind,
+        name: "Redux Toolkit",
+        icon: redux,
     },
     {
-    name: "Node JS",
-    icon: nodejs,
+        name: "Tailwind CSS",
+        icon: tailwind,
     },
     {
-    name: "MongoDB",
-    icon: mongodb,
+        name: "Node JS",
+        icon: nodejs,
     },
     {
-    name: "Three JS",
-    icon: threejs,
+        name: "MongoDB",
+        icon: mongodb,
     },
     {
-    name: "git",
-    icon: git,
+        name: "Three JS",
+        icon: threejs,
     },
     {
-    name: "figma",
-    icon: figma,
+        name: "git",
+        icon: git,
     },
     {
-    name: "docker",
-    icon: docker,
-    },
-    ];
-    const experiences = [
-    {
-    title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
-    iconBg: "#383E56",
-    date: "March 2020 - April 2021",
-    points: [
-    "Developing and maintaining web applications using React.js and other related technologies.",
-    "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-    "Implementing responsive design and ensuring cross-browser compatibility.",
-    "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
+        name: "figma",
+        icon: figma,
     },
     {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: tesla,
-    iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
-    points: [
-    "Developing and maintaining web applications using React.js and other related technologies.",
-    "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-    "Implementing responsive design and ensuring cross-browser compatibility.",
-    "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
+        name: "docker",
+        icon: docker,
+    },
+];
+
+const experiences = [
+    {
+        title: "Technical Analyst Manager",
+        company_name: "Apollo Urgent Care/Carolina Primary Care",
+        icon: apollo,
+        iconBg: "#383E56",
+        date: "May 2021 - August 2023",
+        points: [
+            "Implemented and maintained adherence to HIPAA regulations for managing protected health information (PHI), leveraging data security best practices.",
+            "Stakeholder Collaboration: Facilitated cross-functional collaboration with physicians, clinical staff, payers, and other stakeholders to resolve complex medical billing and coding issues using data-driven solutions.",
+            "Financial Analysis: Utilized data analytics tools to develop and interpret financial metrics, assessing the firm’s fiscal health and performance.",
+            "Billing Cycle Optimization: Conducted advanced analysis of billing cycle data, employing statistical methods and data visualization techniques to support strategic decision-making and operational improvements.",
+            "Reporting and Metrics: Engineered automated reporting systems to generate and visualize key performance indicators (KPIs) such as days in accounts receivable and denial rates, enhancing data accessibility and decision-making efficiency.",
+        ],
+    }, 
+    {   
+        title: "Coding Instructor",
+        company_name: "The Coder School",
+        icon: coderschool,
+        iconBg: "#E6DEDD",
+        date: "May 2024 - August 2024",
+        points: [
+            "Led AI and Python Camps: Delivered engaging, hands-on AI and Python coding camps for middle and high school students, fostering a strong foundation in machine learning concepts and programming skills.",
+            "Curriculum Development: Designed and implemented structured lesson plans, covering core AI algorithms, Python syntax, data manipulation, and real-world applications, tailored to various skill levels.",
+            "Project-Based Learning: Guided students through project-based learning experiences, enabling them to build AI models, develop Python applications, and solve complex problems, enhancing their critical thinking and coding abilities.",
+            "Mentorship and Support: Provided personalized mentorship, offering feedback and support to students, ensuring they grasped key concepts and achieved their learning objectives.",
+            "Integration with Software Engineering Practices: Introduced students to industry-standard tools and best practices in software engineering and data science, preparing them for future academic and career opportunities in technology.",
+        ],
     },
     {
-    title: "Web Developer",
-    company_name: "Shopify",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
-    points: [
-    "Developing and maintaining web applications using React.js and other related technologies.",
-    "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-    "Implementing responsive design and ensuring cross-browser compatibility.",
-    "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
+        title: "Software Engineering Fellow",
+        company_name: "Headstarter AI",
+        icon: headstarter,
+        iconBg: "#E6DEDD",
+        date: "July 2024 - Present",
+        points: [
+            "AI Project Development: Engaged in building and deploying 5 innovative AI projects, applying cutting-edge techniques in machine learning and data science.",
+            "Hackathon Participation: Competed in high-stakes hackathons, showcasing problem-solving skills and collaborative teamwork.",
+            "Startup Growth Initiative: Spearheading a final project to drive significant user engagement by achieving 1,000 people on the waitlist, 1,000 new accounts created, or generating $1,000 in revenue.",
+            "Skills Enhancement: Advanced expertise in AI vision systems, personal branding, and interview preparation through targeted learning and practical experience.",
+            "Networking and Presentation: Strengthened professional connections and presentation skills, preparing for future career opportunities in the tech industry.",
+        ],
+    },
+];
+
+const testimonials = [
+    {
+        testimonial:
+            "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+        name: "Sara Lee",
+        designation: "CFO",
+        company: "Acme Co",
+        image: "https://randomuser.me/api/portraits/women/4.jpg",
     },
     {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-    "Developing and maintaining web applications using React.js and other related technologies.",
-    "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-    "Implementing responsive design and ensuring cross-browser compatibility.",
-    "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-    },
-    ];
-    const testimonials = [
-    {
-    testimonial:
-    "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+        testimonial:
+            "I've never met a web developer who truly cares about their clients' success like Rick does.",
+        name: "Chris Brown",
+        designation: "COO",
+        company: "DEF Corp",
+        image: "https://randomuser.me/api/portraits/men/5.jpg",
     },
     {
-    testimonial:
-    "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
+        testimonial:
+            "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+        name: "Lisa Wang",
+        designation: "CTO",
+        company: "456 Enterprises",
+        image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+];
+
+const projects = [
+    {
+        name: "Car Rent",
+        description:
+            "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "mongodb",
+                color: "green-text-gradient",
+            },
+            {
+                name: "tailwind",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: carrent,
+        source_code_link: "https://github.com/",
     },
     {
-    testimonial:
-    "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
-    ];
-    const projects = [
-    {
-    name: "Car Rent",
-    description:
-    "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-    tags: [
-    {
-    name: "react",
-    color: "blue-text-gradient",
-    },
-    {
-    name: "mongodb",
-    color: "green-text-gradient",
+        name: "Job IT",
+        description:
+            "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "restapi",
+                color: "green-text-gradient",
+            },
+            {
+                name: "scss",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: jobit,
+        source_code_link: "https://github.com/",
     },
     {
-    name: "tailwind",
-    color: "pink-text-gradient",
+        name: "Trip Guide",
+        description:
+            "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+        tags: [
+            {
+                name: "nextjs",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "supabase",
+                color: "green-text-gradient",
+            },
+            {
+                name: "css",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: tripguide,
+        source_code_link: "https://github.com/",
     },
-    ],
-    image: carrent,
-    source_code_link: "https://github.com/",
-    },
-    {
-    name: "Job IT",
-    description:
-    "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-    tags: [
-    {
-    name: "react",
-    color: "blue-text-gradient",
-    },
-    {
-    name: "restapi",
-    color: "green-text-gradient",
-    },
-    {
-    name: "scss",
-    color: "pink-text-gradient",
-    },
-    ],
-    image: jobit,
-    source_code_link: "https://github.com/",
-    },
-    {
-    name: "Trip Guide",
-    description:
-    "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-    tags: [
-    {
-    name: "nextjs",
-    color: "blue-text-gradient",
-    },
-    {
-    name: "supabase",
-    color: "green-text-gradient",
-    },
-    {
-    name: "css",
-    color: "pink-text-gradient",
-    },
-    ],
-    image: tripguide,
-    source_code_link: "https://github.com/",
-    },
-    ];
-    export { services, technologies, experiences, testimonials, projects };
+];
+
+export { services, technologies, experiences, testimonials, projects };
